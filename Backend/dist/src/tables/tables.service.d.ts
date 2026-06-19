9cfa2f9006d7b90007db56c71e49203ff95ea5df
ImaneKr-08 <imane.kari.08@gmail.com>
2026-06-19 13:22:00 +0200

    Revert "idk wallah"
    This reverts commit 653fc3adbeef0514fb72202c75e9d4221fa41c29.

--- Backend/dist/src/tables/tables.service.d.ts
+++ Backend/dist/src/tables/tables.service.d.ts
@@ -8,10 +8,10 @@ export declare class TablesService {
     constructor(prisma: PrismaService, qrCodesService: QrCodesService);
     create(createTableDto: CreateTableDto): Promise<{
         id: number;
-        positionX: number;
-        positionY: number;
         classroomId: number;
         qrCode: string | null;
+        positionX: number;
+        positionY: number;
     } | null>;
     findAll(classroomId?: number): Promise<({
         classroom: {
@@ -24,10 +24,10 @@ export declare class TablesService {
         };
     } & {
         id: number;
-        positionX: number;
-        positionY: number;
         classroomId: number;
         qrCode: string | null;
+        positionX: number;
+        positionY: number;
     })[]>;
     findOne(id: number): Promise<{
         classroom: {
@@ -40,23 +40,23 @@ export declare class TablesService {
         };
     } & {
         id: number;
-        positionX: number;
-        positionY: number;
         classroomId: number;
         qrCode: string | null;
+        positionX: number;
+        positionY: number;
     }>;
     update(id: number, updateTableDto: UpdateTableDto): Promise<{
         id: number;
-        positionX: number;
-        positionY: number;
         classroomId: number;
         qrCode: string | null;
+        positionX: number;
+        positionY: number;
     }>;
     remove(id: number): Promise<{
         id: number;
-        positionX: number;
-        positionY: number;
         classroomId: number;
         qrCode: string | null;
+        positionX: number;
+        positionY: number;
     }>;
 }
